eaae242d172e214b46344a44f00acca2d6f2667d
ryan28561 <ryan@lightningkite.com>
2018-09-19 18:42:17 -0600

    added templatetags for super_collections

--- super_collections/storefront_views.py
+++ super_collections/storefront_views.py
@@ -17,5 +17,5 @@ def super_collection_index(request, slug, pk):
     if super_collection.slug != slug:
         return HttpResponsePermanentRedirect(super_collection.get_absolute_url())
     ctx = {}
-    ctx.update({'super_collection': super_collection})
+    ctx.update({'super_collection': super_collection, 'super_collections': super_collections})
     return TemplateResponse(request, 'super_collections/super_collection.html', ctx)
--- super_collections/templatetags/super_collections.py
+++ super_collections/templatetags/super_collections.py
@@ -2,8 +2,13 @@ from django import template
 
 register = template.Library()
 
+from ..models import SuperCollection
 
 @register.inclusion_tag('super_collections/dashboard/side_nav_inclusion.html',
                         takes_context=True)
 def super_collections_side_nav(context):
     return context
+
+@register.inclusion_tag('super_collections/_list.html')
+def super_collections():
+    return {"super_collections": SuperCollection.objects.all()}

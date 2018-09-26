ecb0a48b77136a13874921e5a0a3ee43df82ece6
722C <722C@users.noreply.github.com>
2018-09-25 18:16:01 -0600

    Additional fields.

--- super_collections/models.py
+++ super_collections/models.py
@@ -54,6 +54,10 @@ class SuperCollection(MPTTModel, SeoModel):
 
     main_picture_1_to_3_box_header_picture = models.ImageField(
         blank=True, null=True)
+    main_picture_1_to_3_box_header_text = models.CharField(
+        max_length=200, blank=True)
+    main_picture_1_to_3_box_subheader_text = models.CharField(
+        max_length=200, blank=True)
     main_picture_1_to_3_box_text = models.CharField(max_length=200, blank=True)
     main_picture_1_to_3_box_button_text = models.CharField(
         max_length=200, blank=True)
@@ -83,6 +87,8 @@ class SuperCollection(MPTTModel, SeoModel):
 
     main_picture_4_box_header_picture = models.ImageField(
         blank=True, null=True)
+    main_picture_4_box_header_text = models.CharField(
+        max_length=200, blank=True)
     main_picture_4_box_text = models.CharField(max_length=200, blank=True)
     main_picture_4_box_button_text = models.CharField(
         max_length=200, blank=True)
